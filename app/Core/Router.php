731950e576d2a8c0b8a6980accb6619d56e2c07c
arthur8409 <arthurvinicius8409@gmali.com>
2025-10-07 19:06:58 -0300

    fix: Corrige parâmetro de roles no Router

--- app/Core/Router.php
+++ app/Core/Router.php
@@ -38,43 +38,50 @@ class Router
         $this->add('POST', 'mesas/liberar', ['controller' => 'MesaController', 'action' => 'liberarMesa']);
         $this->add('GET', 'mesas/detalhes/{id:\d+}', ['controller' => 'MesaController', 'action' => 'showDetalhesMesa']);
     }
-
-    // O resto do seu arquivo continua igual...
     
-    public function add($method, $route, $params = [])
+    /**
+     * AQUI ESTÁ A CORREÇÃO!
+     * Adicionamos $roles = [] como um parâmetro opcional.
+     */
+    public function add($method, $route, $params = [], $roles = [])
     {
-        // ... (código existente) ...
         $route = preg_replace('/\{([a-z]+):([^\}]+)\}/', '(?P<\1>\2)', $route);
         $route = '/^' . str_replace('/', '\/', $route) . '$/i';
+        
         $this->routes[] = [
             'method' => strtoupper($method),
             'route'  => $route,
             'params' => $params,
-            'roles'  => array_map('strtolower', $roles)
+            'roles'  => array_map('strtolower', $roles) // Agora $roles sempre será um array
         ];
     }
-    public function match($url) {
+
+    public function match($url)
+    {
         $current_method = $_SERVER['REQUEST_METHOD'];
         foreach ($this->routes as $routeInfo) {
             if ($routeInfo['method'] === $current_method && preg_match($routeInfo['route'], $url, $matches)) {
                 $this->params = $routeInfo['params'];
-
                 foreach ($matches as $key => $value) {
                     if (is_string($key)) { $this->params[$key] = $value; }
-
                 }
                 return true;
             }
         }
         return false;
     }
-    public function dispatch() {
+    
+    public function dispatch()
+    {
         $url = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
+        
         if ($this->match($url)) {
             $controller = "App\\Controllers\\" . $this->params['controller'];
+            
             if (class_exists($controller)) {
                 $controller_object = new $controller(); 
                 $action = $this->params['action'];
+                
                 if (method_exists($controller_object, $action)) {
                     $controller_object->$action($this->params);
                 } else {
